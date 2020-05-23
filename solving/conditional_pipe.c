d53499945873c30eea42b480a5f8810b3d8ac8b5
Joshua Green <jvg1981@aim.com>
2020-05-22 22:03:17 -0400

    Taking advantage of the known order of the enums.

--- solving/conditional_pipe.c
+++ solving/conditional_pipe.c
@@ -18,6 +18,15 @@ conditional_pipe_solve_return_type conditional_pipe_solve_delegate(slice_index s
   stip_length_type const save_solve_nr_remaining = solve_nr_remaining;
   stip_length_type const save_solve_result_min = solve_result_min();
   stip_length_type const save_solve_result_max = solve_result_max();
+  
+  {
+    enum {
+      ensure_previous_move_is_illegal_plus_1_equals_immobility_on_next_move    = 1/((previous_move_is_illegal+1)==immobility_on_next_move),
+      ensure_immobility_on_next_move_plus_1_equals_previous_move_has_solved    = 1/((immobility_on_next_move+1)==previous_move_has_solved),
+      ensure_previous_move_has_solved_plus_1_equals_next_move_has_solution     = 1/((previous_move_has_solved+1)==next_move_has_solution),
+      ensure_next_move_has_solution_plus_1_equals_previous_move_has_not_solved = 1/((next_move_has_solution+1)==previous_move_has_not_solved)
+    };
+  }
 
   TraceFunctionEntry(__func__);
   TraceFunctionParam("%u",si);
@@ -29,9 +38,13 @@ conditional_pipe_solve_return_type conditional_pipe_solve_delegate(slice_index s
   result.result_min = solve_result_min();
   if (result.result_min < previous_move_is_illegal)
     result.result_min = previous_move_is_illegal;
+  else if (result.result_min == next_move_has_solution)
+    ++result.result_min;
   result.result_max = solve_result_max();
   if (result.result_max > previous_move_has_not_solved)
     result.result_max = previous_move_has_not_solved;
+  else if (result.result_max == next_move_has_solution)
+    --result.result_max;
   assert(result.result_min <= result.result_max);
 
   solve_nr_remaining = save_solve_nr_remaining;
